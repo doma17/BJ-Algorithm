6b1b26bccb5f5465e00cca5b878341d638a92919
kbm <rovin1273@gmail.com>
2023-01-20 15:09:38 +0900

    third commit

--- Week4_DP/15989/15989.cpp
+++ Week4_DP/15989/15989.cpp
@@ -7,25 +7,25 @@ typedef long long int ll;
 
 int main() {
     fastio;
-    int T, n, MOD = 1000000009;
+    int T, n;
     cin >> T;
-
-    priority_queue<int> q;
     vector<int> v;
+    priority_queue<int> q;
     while(T--) {
         cin >> n;
-        q.push(n);
         v.push_back(n);
+        q.push(n);
     }
-
-    ll dp[q.top() + 1][3];
-    dp[1][0] = 1;
-    dp[2][0] = 1; dp[2][1] = 1;
-    dp[3][0] = 1; dp[3][1] = 1; dp[3][2] = 1;
-    for(int i = 4; i <= q.top(); ++i) {
-        dp[i][0] = dp[i-1][0];
-        dp[i][1] = dp[i-2][0] + dp[i-2][1];
-        dp[i][2] = dp[i-3][0] + dp[i-3][1] + dp[i-3][2];
-    } 
-    for(auto x : v) cout << dp[x][0] + dp[x][1] + dp[x][2] << "\n";
+    
+    ll dp[q.top() + 1][4];
+    dp[0][1] = 1; dp[0][2] = 0; dp[0][3] = 0;
+    dp[1][1] = 1; dp[1][2] = 0; dp[1][3] = 0;
+    for(int i = 2; i <= q.top(); i++) {
+        dp[i][1] = dp[i-1][1];
+        dp[i][2] = dp[i-2][1] + dp[i-2][2];
+        if(i >= 3) dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+    }
+    for(auto x : v) {
+        cout << dp[x][1] + dp[x][2] + dp[x][3] << "\n";
+    }
 }
