fa99966b05944d887ce479eacadf38acdecf8987
kbm <rovin1273@gmail.com>
2023-01-24 23:54:52 +0900

    2023-01-24-23-54

---
+++ Week4_DP/2293/2293.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+int main() {
+    fastio;
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for(auto x : v) cin >> x;
+
+    
+}
