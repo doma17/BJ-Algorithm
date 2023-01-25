a8fdac8afb5293ff8347ecb8a3f33ddd4a00fd48
kbm <rovin1273@gmail.com>
2023-01-25 13:19:32 +0900

    2023-01-25-13-19

--- Week4_DP/2624/2624.cpp
+++ Week4_DP/2624/2624.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
 int main() {
     fastio;
-    int n, k;
-    cin >> n >> k;
-    int coin[n]; int dp[10001];
-    for(int i = 0; i < n; i++) cin >> coin[i];
-    for(int i = 0; i <= k; i++) dp[i] = 100001;
-    
+    int T, k;
+    cin >> T >> k;
+    int coin[k][2];
+    for(int i = 0; i < k; i++) cin >> coin[i][0] >> coin[i][1];
 
-    dp[0] = 0;
-    for(int i = 0; i < n; i++) {
-        for(int j = coin[i]; j <= k; j++) {
-            dp[j] = min(dp[j], dp[j - coin[i]] + 1);
+    int dp[10001] = {0}; dp[0] = 1;
+    for(int i = 0; i < k; i++) {
+        int coin_val = coin[i][0];
+        int coin_cnt = coin[i][1];
+
+        // 인덱스 1부터 dp를 계산하게 되면 중복으로 값이 들어가기 때문에 T에서 부터 1씩 내려온다.
+        for(int j = T; j >= 0; j--) {
+            for(int p = 1; p <= coin_cnt && j - coin_val * p >= 0; p++) {
+                dp[j] += dp[j - coin_val * k];
+            }
         }
     }
-    if(dp[k] == 100001) cout << "-1\n";
-    else cout << dp[k] << "\n";
+    cout << dp[T] << "\n";
 }
