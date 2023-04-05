cf800a044e469905ad88d0f6a30a3e8971933cca
kbm <rovin1273@gmail.com>
2023-04-05 16:10:26 +0900

    2023-4-5-16-10

--- Week11_MST_tSort/2887/2887.cpp
+++ Week11_MST_tSort/2887/2887.cpp
@@ -21,6 +21,7 @@ vector<tuple<int, int, int>> edge; // {cost, node1, node2}
 int parent[100001];
 ll ans;
 
+// 최종적으로 재귀를 통해서 연결 되어있는 모든 노드들의 부모 노드를 찾는 함수
 int find(int x) {
     if(parent[x] == x) return x;
     return parent[x] = find(parent[x]);
@@ -31,23 +32,23 @@ bool same(int x, int y) {
     x = find(x);
     y = find(y);
     if(x == y) return false;
-    else {
-        parent[x] = y;
-        return true;
-    }
+    parent[x] = y;
+    return true;
 }
 
 void union_find() {
+    // 기본적으로 초기값은 자신의 부모노드를 자기 자신 노드로 설정한다.
     for(int i = 0; i < n; i++) {
         parent[i] = i;
     }
 
-    int a, b, c;
+    // union_find 기법을 통해서 연결한다.
+    int a, b, cost;
     for(auto e : edge) {
-        tie(c, a, b) = e;
+        tie(cost, a, b) = e;
         
         if(same(a, b))
-            ans += c;
+            ans += cost;
     }
 }
 
@@ -63,10 +64,12 @@ int main() {
         Z.push_back({z, i});
     }
 
+    // 정렬을 통해서 가장 가까운 거리 값을 index + 1, index - 1 로 정리한다.
     sort(X.begin(), X.end());
     sort(Y.begin(), Y.end());
     sort(Z.begin(), Z.end());
 
+    // edge -> {cost, node1, node2}
     for(int i = 0; i < n-1; i++) {
         edge.push_back({abs(X[i].first - X[i + 1].first), X[i].second, X[i + 1].second});
         edge.push_back({abs(Y[i].first - Y[i + 1].first), Y[i].second, Y[i + 1].second});
---
+++ Week12/2003/2003.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define INF 987654321
+typedef long long int ll;
+
+int main() {
+	fastio;
+
+    
+}
