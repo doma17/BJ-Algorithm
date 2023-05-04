4c2c030b55de266f04507f211e799974e24f5952
kbm <rovin1273@gmail.com>
2023-05-04 16:05:16 +0900

    2023-5-4-16-04

---
+++ Week13_TreeBasic/1068/1068.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define INF 987654321
+typedef long long int ll;
+
+// 백준 2003번 문제
+
+int main() {
+	fastio;
+    
+}
